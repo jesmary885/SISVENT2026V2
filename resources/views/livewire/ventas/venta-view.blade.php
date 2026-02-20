6f7f1b238e96b942dc42ad07c8c70872c356d458
jesmary885 <jesmary885@gmail.com>
2026-02-20 18:09:58 -0400

    20-2-26

--- resources/views/livewire/ventas/venta-view.blade.php
+++ resources/views/livewire/ventas/venta-view.blade.php
@@ -108,7 +108,7 @@
                                     <tr class="hover:bg-gray-50 transition-colors">
                                         <td class="py-4 px-4">
                                             <div>
-                                                <p class="font-medium text-gray-900">{{ $producto->producto_presentacion->producto->nombre }}</p>
+                                                <p class="font-medium text-gray-900">{{ $producto->producto_presentacion->producto->nombre }} ({{ $producto->producto_presentacion->nombre }})</p>
                                             </div>
                                         </td>
                                         <td class="py-4 px-4 text-center">
